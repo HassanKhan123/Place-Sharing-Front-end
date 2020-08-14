870d5c9ef3bf3d19b3cda56858b64bf42f71a4dd
HassanKhan123 <hk2602809@gmail.com>
2020-08-14 18:05:40 +0500

    starting setup, Pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-function App() {
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+
+const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Users} />
+        <Route exact path="/places/new" component={NewPlace} />
+
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewPlace = () => {
+    return (
+        <div>
+            <h2>New Place Works</h2>
+        </div>
+    )
+}
+
+export default NewPlace
