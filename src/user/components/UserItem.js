025166c5a687e67cd29c2281f614250dea90f683
HassanKhan123 <hk2602809@gmail.com>
2020-08-14 20:10:05 +0500

    styling added

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,22 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
 import "./UserItem.css";
 
 const UserItem = ({ user }) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={user.image} alt={user.name} />
-        </div>
+      
+        <Card className="user-item__content">
+          <Link to={`/${user.id}/places`}>
+            <div className="user-item__image">
+              <Avatar image={user.image} alt={user.name} />
+            </div>
 
-        <div className="user-item__info">
-          <h2>{user.name}</h2>
-          <h3>
-            {user.places} {user.places === 1 ? "Place" : "Places"}
-          </h3>
-        </div>
-      </div>
+            <div className="user-item__info">
+              <h2>{user.name}</h2>
+              <h3>
+                {user.places} {user.places === 1 ? "Place" : "Places"}
+              </h3>
+            </div>
+          </Link>
+        </Card>
+      
     </li>
   );
 };
