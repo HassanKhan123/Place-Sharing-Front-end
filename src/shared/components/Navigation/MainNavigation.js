27cbbd5c19e74cf127214e396d3aa37511e1fbb4
HassanKhan123 <hk2602809@gmail.com>
2020-08-14 21:27:33 +0500

    portal added

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -2,25 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
-import NavLinks from './NavLinks'
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 import "./MainNavigation.css";
 
 const MainNavigation = () => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
+    <>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
 
-      <h1 className="main-navigation__title">
-        <Link to="/">Your Places</Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
   );
 };
 
