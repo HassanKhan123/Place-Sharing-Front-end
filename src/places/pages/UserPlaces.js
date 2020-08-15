b5ad3da9837476b74772e8b514a0977c3ab3d005
HassanKhan123 <hk2602809@gmail.com>
2020-08-15 13:44:53 +0500

    route params added

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,37 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
 
-import PlaceList from '../components/PlaceList'
+import PlaceList from "../components/PlaceList";
 
-const DUMMY_PLACES=[
-    {
-        id:'p1',
-        title:'Empire State Builing',
-        description:'One of the most famous sky scrapers in the world',
-        image:'https://images.pexels.com/photos/472037/pexels-photo-472037.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        address:'20 W 34th St, New York, NY 10001',
-        location:{
-            lat:40.7485492,
-            lng:-73.9879522,
-        },
-        creator:'u1'
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Builing",
+    description: "One of the most famous sky scrapers in the world",
+    image:
+      "https://images.pexels.com/photos/472037/pexels-photo-472037.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7485492,
+      lng: -73.9879522,
     },
-    {
-        id:'p2',
-        title:'Empire State Builing',
-        description:'One of the most famous sky scrapers in the world',
-        image:'https://images.pexels.com/photos/472037/pexels-photo-472037.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        address:'20 W 34th St, New York, NY 10001',
-        location:{
-            lat:40.7485492,
-            lng:-73.9879522,
-        },
-        creator:'u2'
-    }
-]
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Builing",
+    description: "One of the most famous sky scrapers in the world",
+    image:
+      "https://images.pexels.com/photos/472037/pexels-photo-472037.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7485492,
+      lng: -73.9879522,
+    },
+    creator: "u2",
+  },
+];
 
 const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
 
-    return <PlaceList items={DUMMY_PLACES}/>
-}
+  return <PlaceList items={loadedPlaces} />;
+};
 
-export default UserPlaces
+export default UserPlaces;
