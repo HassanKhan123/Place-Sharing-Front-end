832e144a6abda719d12e2a5f11f9f91de1fce14a
HassanKhan123 <hk2602809@gmail.com>
2020-08-16 18:40:30 +0500

    update place form added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from './places/pages/UpdatePlace'
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
@@ -20,6 +21,7 @@ const App = () => {
           <Route exact path="/" component={Users} />
           <Route exact path="/:userId/places" component={UserPlaces} />
           <Route exact path="/places/new" component={NewPlace} />
+          <Route exact path="/places/:placeId" component={UpdatePlace} />
           <Redirect to="/" />
         </Switch>
       </main>
---
+++ src/places/pages/UpdatePlace.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/utils/validators";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Builing",
+    description: "One of the most famous sky scrapers in the world",
+    image:
+      "https://images.pexels.com/photos/472037/pexels-photo-472037.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7485492,
+      lng: -73.9879522,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Builing",
+    description: "One of the most famous sky scrapers in the world",
+    image:
+      "https://images.pexels.com/photos/472037/pexels-photo-472037.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7485492,
+      lng: -73.9879522,
+    },
+    creator: "u2",
+  },
+];
+
+const UpdatePlace = () => {
+  const placeId = useParams().placeId;
+  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <h2>Could not find place!</h2>
+      </div>
+    );
+  }
+  return (
+    <form>
+      <Input
+        id="title"
+        type="text"
+        label="Title"
+        element="input"
+        errorText="Please enter a valid title."
+        validators={[VALIDATOR_REQUIRE()]}
+        onInput={() => {}}
+        value={identifiedPlace.title}
+        isValid={true}
+      />
+      <Input
+        id="description"
+        label="Description"
+        element="textarea"
+        errorText="Please enter a valid description (atleast 5 characters)."
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        onInput={() => {}}
+        value={identifiedPlace.description}
+        isValid={true}
+      />
+
+      <Button type="submit" disabled={true}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;
