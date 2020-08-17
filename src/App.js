4b1469ed610cab561f827f9e0cc450835a7d85ef
HassanKhan123 <hk2602809@gmail.com>
2020-08-17 07:27:43 +0500

    added auth-context for app-wide state management

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -9,25 +9,36 @@ import {
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
-import UpdatePlace from './places/pages/UpdatePlace'
-import Auth from './user/pages/Auth'
+import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import { AuthContext } from "./shared/context/auth-context";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route exact path="/" component={Users} />
-          <Route exact path="/:userId/places" component={UserPlaces} />
-          <Route exact path="/places/new" component={NewPlace} />
-          <Route exact path="/places/:placeId" component={UpdatePlace} />
-          <Route exact path="/auth" component={Auth}/>
-          <Redirect to="/" />
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      <Router>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Route exact path="/" component={Users} />
+            <Route exact path="/:userId/places" component={UserPlaces} />
+            <Route exact path="/places/new" component={NewPlace} />
+            <Route exact path="/places/:placeId" component={UpdatePlace} />
+            <Route exact path="/auth" component={Auth} />
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
